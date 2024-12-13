b2da5fc75e80aae22452c6c38f286595ed2ce9fc
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-12-14 00:25:48 +0300

    product entity added

---
+++ locking/src/main/java/com/ofg/locking/model/Product.java
@@ -0,0 +1,23 @@
+package com.ofg.locking.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long price;
+
+    @Version
+    private Integer version;
+}
