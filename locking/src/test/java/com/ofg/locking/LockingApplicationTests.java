6e6d59bb5bcc204276056ca2b72b434f7fa543c5
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-12-13 23:33:06 +0300

    initial commit for locking

---
+++ locking/src/test/java/com/ofg/locking/LockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofg.locking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
