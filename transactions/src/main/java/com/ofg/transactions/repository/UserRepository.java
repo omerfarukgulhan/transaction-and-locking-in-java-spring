53ea2bfb351569d964ba7a3a4c7fb008986965a9
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-12-13 23:30:07 +0300

    user repo added

---
+++ transactions/src/main/java/com/ofg/transactions/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ofg.transactions.repository;
+
+import com.ofg.transactions.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
