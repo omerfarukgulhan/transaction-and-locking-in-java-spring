6fa6349f05b9a89b144c1959042cf8695b2abab2
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-12-14 00:27:07 +0300

    seed data added in main file

--- locking/src/main/java/com/ofg/locking/LockingApplication.java
+++ locking/src/main/java/com/ofg/locking/LockingApplication.java
@@ -1,13 +1,26 @@
 package com.ofg.locking;
 
+import com.ofg.locking.model.Product;
+import com.ofg.locking.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LockingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LockingApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(ProductRepository productRepository) {
+        return args -> {
+            Product product1 = new Product(null, "Product 1", 100L, null);
+            Product product2 = new Product(null, "Product 2", 200L, null);
+
+            productRepository.save(product1);
+            productRepository.save(product2);
+        };
+    }
 }
